2971012145d501338f72814e5e6fbcfa6809a9bc
Manish Ranjan <97095359+Indian24@users.noreply.github.com>
2026-01-22 17:55:04 +0530

    Create secrets.example.h

---
+++ secrets.example.h
@@ -0,0 +1,11 @@
+// Rename this file to secrets.h before use
+// DO NOT commit secrets.h to GitHub
+
+#define WIFI_SSID "Your_WiFi_Name"
+#define WIFI_PASSWORD "Your_WiFi_Password"
+#define BLYNK_AUTH_TOKEN "Your_Blynk_Token"
+
+👉 In your .ino file later you can do:
+Copy code
+Cpp
+#include "secrets.h"
